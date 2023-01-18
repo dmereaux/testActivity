ae075d55ba4aed53315f776f1f0d73f0de42d1d6
dominique.mereaux <dmereaux@gmail.com>
2023-01-18 20:43:28 +0100

    nouveau TC

---
+++ Scripts/TC3/Script1674061547941.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('/Users/dominiquemereaux/Katalon Studio/toto/androidapp/APIDemos.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.Button - OK (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - Graphics'), 0)
+
+Mobile.takeScreenshot('/var/folders/3b/z_3bn4n12j59v3g7l9pm1x8r0000gn/T/screenshot8138161755585104391.png')
+
+Mobile.closeApplication()
+
